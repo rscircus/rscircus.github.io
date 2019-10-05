d44ca76a083d68789615861a0af90f8501ef7ebb
Roland Siegbert <roland@siegbert.info>
2019-10-05 22:28:57 +0200

    Add touch support

--- pages/climate_anomaly/globe/globe.js
+++ pages/climate_anomaly/globe/globe.js
@@ -15,62 +15,66 @@ var DAT = DAT || {};
 
 DAT.Globe = function(container, opts) {
   opts = opts || {};
-  
-  var colorFn = opts.colorFn || function(x) {
-    var c = new THREE.Color();
-    if ( x > 0 ) { //warming
-      //c.setHSL( ( 0.6 + ( x * 0.5 ) ), 1.0, 0.5 );
-      c.setHSL( 1.0, 0.7, 0.3 );
-    } else { // cooling
-      c.setHSL( 0.6, 0.7, 0.3 );
-    }
-    return c;
-  };
+
+  var colorFn =
+    opts.colorFn ||
+    function(x) {
+      var c = new THREE.Color();
+      if (x > 0) {
+        //warming
+        //c.setHSL( ( 0.6 + ( x * 0.5 ) ), 1.0, 0.5 );
+        c.setHSL(1.0, 0.7, 0.3);
+      } else {
+        // cooling
+        c.setHSL(0.6, 0.7, 0.3);
+      }
+      return c;
+    };
   var imgDir = opts.imgDir || 'globe/';
 
   var Shaders = {
-    'earth' : {
+    earth: {
       uniforms: {
-        'texture': { type: 't', value: null }
+        texture: {type: 't', value: null},
       },
       vertexShader: [
         'varying vec3 vNormal;',
         'varying vec2 vUv;',
         'void main() {',
-          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-          'vNormal = normalize( normalMatrix * normal );',
-          'vUv = uv;',
-        '}'
+        'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        'vNormal = normalize( normalMatrix * normal );',
+        'vUv = uv;',
+        '}',
       ].join('\n'),
       fragmentShader: [
         'uniform sampler2D texture;',
         'varying vec3 vNormal;',
         'varying vec2 vUv;',
         'void main() {',
-          'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-          'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
-          'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
-        '}'
-      ].join('\n')
+        'vec3 diffuse = texture2D( texture, vUv ).xyz;',
+        'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+        'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+        'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
+        '}',
+      ].join('\n'),
     },
-    'atmosphere' : {
+    atmosphere: {
       uniforms: {},
       vertexShader: [
         'varying vec3 vNormal;',
         'void main() {',
-          'vNormal = normalize( normalMatrix * normal );',
-          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-        '}'
+        'vNormal = normalize( normalMatrix * normal );',
+        'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        '}',
       ].join('\n'),
       fragmentShader: [
         'varying vec3 vNormal;',
         'void main() {',
-          'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
-        '}'
-      ].join('\n')
-    }
+        'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
+        'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+        '}',
+      ].join('\n'),
+    },
   };
 
   var camera, scene, renderer, w, h;
@@ -81,17 +85,18 @@ DAT.Globe = function(container, opts) {
   var curZoomSpeed = 0;
   var zoomSpeed = 50;
 
-  var mouse = { x: 0, y: 0 }, mouseOnDown = { x: 0, y: 0 };
-  var rotation = { x: 0, y: 0 },
-      target = { x: Math.PI*3/2, y: Math.PI / 6.0 },
-      targetOnDown = { x: 0, y: 0 };
+  var mouse = {x: 0, y: 0},
+    mouseOnDown = {x: 0, y: 0};
+  var rotation = {x: 0, y: 0},
+    target = {x: (Math.PI * 3) / 2, y: Math.PI / 6.0},
+    targetOnDown = {x: 0, y: 0};
 
-  var distance = 100000, distanceTarget = 100000;
+  var distance = 150000,
+    distanceTarget = 150000;
   var padding = 40;
   var PI_HALF = Math.PI / 2;
 
   function init() {
-
     container.style.color = '#fff';
     container.style.font = '13px/20px Arial, sans-serif';
 
@@ -109,15 +114,15 @@ DAT.Globe = function(container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir+'world.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture(
+      imgDir + 'world.jpg',
+    );
 
     material = new THREE.ShaderMaterial({
-
-          uniforms: uniforms,
-          vertexShader: shader.vertexShader,
-          fragmentShader: shader.fragmentShader
-
-        });
+      uniforms: uniforms,
+      vertexShader: shader.vertexShader,
+      fragmentShader: shader.fragmentShader,
+    });
 
     mesh = new THREE.Mesh(geometry, material);
     mesh.rotation.y = Math.PI;
@@ -127,22 +132,20 @@ DAT.Globe = function(container, opts) {
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
     material = new THREE.ShaderMaterial({
-
-          uniforms: uniforms,
-          vertexShader: shader.vertexShader,
-          fragmentShader: shader.fragmentShader,
-          side: THREE.BackSide,
-          blending: THREE.AdditiveBlending,
-          transparent: true
-
-        });
+      uniforms: uniforms,
+      vertexShader: shader.vertexShader,
+      fragmentShader: shader.fragmentShader,
+      side: THREE.BackSide,
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+    });
 
     mesh = new THREE.Mesh(geometry, material);
-    mesh.scale.set( 1.1, 1.1, 1.1 );
+    mesh.scale.set(1.1, 1.1, 1.1);
     scene.add(mesh);
 
     geometry = new THREE.BoxGeometry(0.75, 0.75, 1);
-    geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,0,-0.5));
+    geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0, -0.5));
 
     point = new THREE.Mesh(geometry);
 
@@ -154,20 +157,27 @@ DAT.Globe = function(container, opts) {
     container.appendChild(renderer.domElement);
 
     container.addEventListener('mousedown', onMouseDown, false);
-
     container.addEventListener('mousewheel', onMouseWheel, false);
-
+    // touch devices:
+    container.addEventListener('touchstart', onMouseDown, false);
+    container.addEventListener('touchmove', onMouseMove, false);
+    container.addEventListener('touchend', onMouseUp, false);
     document.addEventListener('keydown', onDocumentKeyDown, false);
-
     window.addEventListener('resize', onWindowResize, false);
-
-    container.addEventListener('mouseover', function() {
-      overRenderer = true;
-    }, false);
-
-    container.addEventListener('mouseout', function() {
-      overRenderer = false;
-    }, false);
+    container.addEventListener(
+      'mouseover',
+      function() {
+        overRenderer = true;
+      },
+      false,
+    );
+    container.addEventListener(
+      'mouseout',
+      function() {
+        overRenderer = false;
+      },
+      false,
+    );
   }
 
   function addData(data, opts, scaling) {
@@ -178,12 +188,16 @@ DAT.Globe = function(container, opts) {
     opts.format = opts.format || 'magnitude'; // other option is 'legend'
     if (opts.format === 'magnitude') {
       step = 3;
-      colorFnWrapper = function(data, i) { return colorFn(data[i+2]); }
+      colorFnWrapper = function(data, i) {
+        return colorFn(data[i + 2]);
+      };
     } else if (opts.format === 'legend') {
       step = 4;
-      colorFnWrapper = function(data, i) { return colorFn(data[i+3]); }
+      colorFnWrapper = function(data, i) {
+        return colorFn(data[i + 3]);
+      };
     } else {
-      throw('error: format not supported: '+opts.format);
+      throw 'error: format not supported: ' + opts.format;
     }
 
     if (opts.animated) {
@@ -192,69 +206,79 @@ DAT.Globe = function(container, opts) {
         for (i = 0; i < data.length; i += step) {
           lat = data[i];
           lng = data[i + 1];
-//        size = data[i + 2];
-          color = colorFnWrapper(data,i);
+          //        size = data[i + 2];
+          color = colorFnWrapper(data, i);
           size = 0;
           addPoint(lat, lng, size, color, this._baseGeometry);
         }
       }
-      if(this._morphTargetId === undefined) {
+      if (this._morphTargetId === undefined) {
         this._morphTargetId = 0;
       } else {
         this._morphTargetId += 1;
       }
-      opts.name = opts.name || 'morphTarget'+this._morphTargetId;
+      opts.name = opts.name || 'morphTarget' + this._morphTargetId;
     }
     var subgeo = new THREE.Geometry();
     for (i = 0; i < data.length; i += step) {
       lat = data[i];
       lng = data[i + 1];
-      color = colorFnWrapper(data,i);
+      color = colorFnWrapper(data, i);
       size = data[i + 2];
-      size = size < 0 ? - size * scaling : size * scaling; // flip negative scale
+      size = size < 0 ? -size * scaling : size * scaling; // flip negative scale
       size = size > 100 ? 0 : size; // filter errors
       addPoint(lat, lng, size, color, subgeo);
     }
     if (opts.animated) {
-      this._baseGeometry.morphTargets.push({'name': opts.name, vertices: subgeo.vertices});
+      this._baseGeometry.morphTargets.push({
+        name: opts.name,
+        vertices: subgeo.vertices,
+      });
     } else {
       this._baseGeometry = subgeo;
     }
-
-  };
+  }
 
   function createPoints() {
     if (this._baseGeometry !== undefined) {
       if (this.is_animated === false) {
-        this.points = new THREE.Mesh(this._baseGeometry, new THREE.MeshBasicMaterial({
-              color: 0xffffff,
-              vertexColors: THREE.FaceColors,
-              morphTargets: false
-            }));
+        this.points = new THREE.Mesh(
+          this._baseGeometry,
+          new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            vertexColors: THREE.FaceColors,
+            morphTargets: false,
+          }),
+        );
       } else {
         if (this._baseGeometry.morphTargets.length < 8) {
-          console.log('t l',this._baseGeometry.morphTargets.length);
-          var padding = 8-this._baseGeometry.morphTargets.length;
+          console.log('t l', this._baseGeometry.morphTargets.length);
+          var padding = 8 - this._baseGeometry.morphTargets.length;
           console.log('padding', padding);
-          for(var i=0; i<=padding; i++) {
-            console.log('padding',i);
-            this._baseGeometry.morphTargets.push({'name': 'morphPadding'+i, vertices: this._baseGeometry.vertices});
+          for (var i = 0; i <= padding; i++) {
+            console.log('padding', i);
+            this._baseGeometry.morphTargets.push({
+              name: 'morphPadding' + i,
+              vertices: this._baseGeometry.vertices,
+            });
           }
         }
-        this.points = new THREE.Mesh(this._baseGeometry, new THREE.MeshBasicMaterial({
-              color: 0xffffff,
-              vertexColors: THREE.FaceColors,
-              morphTargets: true
-            }));
+        this.points = new THREE.Mesh(
+          this._baseGeometry,
+          new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            vertexColors: THREE.FaceColors,
+            morphTargets: true,
+          }),
+        );
       }
       scene.add(this.points);
     }
   }
 
   function addPoint(lat, lng, size, color, subgeo) {
-
-    var phi = (90 - lat) * Math.PI / 180;
-    var theta = (180 - lng) * Math.PI / 180;
+    var phi = ((90 - lat) * Math.PI) / 180;
+    var theta = ((180 - lng) * Math.PI) / 180;
 
     point.position.x = 200 * Math.sin(phi) * Math.cos(theta);
     point.position.y = 200 * Math.cos(phi);
@@ -262,15 +286,13 @@ DAT.Globe = function(container, opts) {
 
     point.lookAt(mesh.position);
 
-    point.scale.z = Math.max( size, 0.1 ); // avoid non-invertible matrix
+    point.scale.z = Math.max(size, 0.1); // avoid non-invertible matrix
     point.updateMatrix();
 
     for (var i = 0; i < point.geometry.faces.length; i++) {
-
       point.geometry.faces[i].color = color;
-
     }
-    if(point.matrixAutoUpdate){
+    if (point.matrixAutoUpdate) {
       point.updateMatrix();
     }
     subgeo.merge(point.geometry, point.matrix);
@@ -287,27 +309,42 @@ DAT.Globe = function(container, opts) {
     container.addEventListener('mouseup', onMouseUp, false);
     container.addEventListener('mouseout', onMouseOut, false);
 
-    mouseOnDown.x = - event.clientX;
-    mouseOnDown.y = event.clientY;
+    if (event.touches.length === 1) { //one-finger touch
+      mouseOnDown.x = -event.touches[0].pageX;
+      mouseOnDown.y = event.touches[0].pageY;
 
-    targetOnDown.x = target.x;
-    targetOnDown.y = target.y;
+      targetOnDown.x = target.x;
+      targetOnDown.y = target.y;
+    } else {
+      mouseOnDown.x = -event.clientX;
+      mouseOnDown.y = event.clientY;
+
+      targetOnDown.x = target.x;
+      targetOnDown.y = target.y;
+    }
 
     container.style.cursor = 'move';
   }
 
   function onMouseMove(event) {
-    mouse.x = - event.clientX;
-    mouse.y = event.clientY;
+    if (event.touches.length === 1) { //one-finger touch
+      mouse.x = -event.touches[0].pageX;
+      mouse.y = event.touches[0].pageY;
+    } else {
+      mouse.x = -event.clientX;
+      mouse.y = event.clientY;
+    }
 
-    var zoomDamp = distance/1000;
+    var zoomDamp = distance / 1000;
 
     target.x = targetOnDown.x + (mouse.x - mouseOnDown.x) * 0.005 * zoomDamp;
     target.y = targetOnDown.y + (mouse.y - mouseOnDown.y) * 0.005 * zoomDamp;
 
     target.y = target.y > PI_HALF ? PI_HALF : target.y;
-    target.y = target.y < - PI_HALF ? - PI_HALF : target.y;
+    target.y = target.y < -PI_HALF ? -PI_HALF : target.y;
   }
+
+  function onTouchMove(event) {}
 
   function onMouseUp(event) {
     container.removeEventListener('mousemove', onMouseMove, false);
@@ -343,10 +380,10 @@ DAT.Globe = function(container, opts) {
     }
   }
 
-  function onWindowResize( event ) {
+  function onWindowResize(event) {
     camera.aspect = container.offsetWidth / container.offsetHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( container.offsetWidth, container.offsetHeight );
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
   }
 
   function zoom(delta) {
@@ -379,7 +416,6 @@ DAT.Globe = function(container, opts) {
   init();
   this.animate = animate;
 
-
   this.__defineGetter__('time', function() {
     return this._time || 0;
   });
@@ -387,16 +423,16 @@ DAT.Globe = function(container, opts) {
   this.__defineSetter__('time', function(t) {
     var validMorphs = [];
     var morphDict = this.points.morphTargetDictionary;
-    for(var k in morphDict) {
-      if(k.indexOf('morphPadding') < 0) {
+    for (var k in morphDict) {
+      if (k.indexOf('morphPadding') < 0) {
         validMorphs.push(morphDict[k]);
       }
     }
     validMorphs.sort();
-    var l = validMorphs.length-1;
-    var scaledt = t*l+1;
+    var l = validMorphs.length - 1;
+    var scaledt = t * l + 1;
     var index = Math.floor(scaledt);
-    for (i=0;i<validMorphs.length;i++) {
+    for (i = 0; i < validMorphs.length; i++) {
       this.points.morphTargetInfluences[validMorphs[i]] = 0;
     }
     var lastIndex = index - 1;
@@ -414,6 +450,4 @@ DAT.Globe = function(container, opts) {
   this.scene = scene;
 
   return this;
-
 };
-
